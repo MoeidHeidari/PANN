ff7af3bb7c83d19a01274305b3223639b57e2319
moeidheidari <moeidheidari@hotmail.com>
2020-08-26 01:02:57 +0300

    Cmake gonfiguration

---
+++ build/CMakeFiles/PANN.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PANN.dir/main.cpp.o"
+  "PANN"
+  "PANN.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PANN.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/moeidheidari/Google_Drive/MACFlies/Projects/PANN
+# Build directory: /Users/moeidheidari/Google_Drive/MACFlies/Projects/PANN/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
