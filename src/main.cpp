14e4595efc1439fc999413afb944f412e2ba1094
moeidheidari <moeidheidari@hotmail.com>
2020-08-26 03:51:13 +0300

    .

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
-//#include <boost/math/special_functions/sign.hpp>
+#include <boost/math/special_functions/sign.hpp>
 #include "lib/include/Sample.h"
 
 int main(int, char**) 
 {
-    //std::cout << copysign(4.9, 7.9) << std::endl;
+    std::cout << copysign(4.9, 7.9) << std::endl;
     
     Sample s;
     s.testFunction();
