14ed28a873f9d2c0b0ad8dd33d9f94f2fb1fedd6
moeidheidari <moeidheidari@hotmail.com>
2020-08-26 03:42:41 +0300

    .

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/moeidheidari/Google_Drive/MACFlies/Projects/PANN
-# Build directory: /Users/moeidheidari/Google_Drive/MACFlies/Projects/PANN/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
